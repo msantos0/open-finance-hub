70773960c5fc67a039de3fa1d4f9274a7284af43
Marcio <marcio.santos.mj@gmail.com>
2026-09-07 11:39:48 -0300

    feat: initial spring boot project

---
+++ src/main/java/com/marcio/open_finance_hub/OpenFinanceHubApplication.java
@@ -0,0 +1,13 @@
+package com.marcio.open_finance_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFinanceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFinanceHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcio/open_finance_hub/OpenFinanceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcio.open_finance_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFinanceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
